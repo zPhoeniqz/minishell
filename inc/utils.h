44cd4030e4b2efaf2a78bee4d28afa6f05cb30f1
Paul Bindl <pbindl@c1b3c2.42berlin.de>
2026-04-21 19:41:16 +0200

    Start work on parser

--- inc/utils.h
+++ inc/utils.h
@@ -6,7 +6,7 @@
 /*   By: pbindl <pbindl@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/17 19:41:36 by pbindl            #+#    #+#             */
-/*   Updated: 2026/04/10 18:44:24 by pbindl           ###   ########.fr       */
+/*   Updated: 2026/04/15 15:11:23 by pbindl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ void	*ft_realloc(void *ptr, size_t newsize);
 void	arr_destroy(void **arr);
 void	ft_env_destroy(char **envp);
 void	ft_env_make_individual_alloc(char **envp);
-int		find_env(const char *name);
-int		ft_setenv(const char *name, const char *value, bool rewrite);
+int		find_env(const char **envp, const char *name);
+int		ft_setenv(char **envp, const char *name, const char *value,
+			bool rewrite);
 
 #endif
